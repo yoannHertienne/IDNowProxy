c06ce4bd7816ee3b70e374615e1ae106dd92be1c
yoannHertienne <yoann.hertienne@accenture.com>
2021-04-14 17:21:38 +0200

    Initial Commit

---
+++ IDNowProxy/IDNowProxy.h
@@ -0,0 +1,18 @@
+//
+//  IDNowProxy.h
+//  IDNowProxy
+//
+//  Created by Hertienne, Yoann on 14/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IDNowProxy.
+FOUNDATION_EXPORT double IDNowProxyVersionNumber;
+
+//! Project version string for IDNowProxy.
+FOUNDATION_EXPORT const unsigned char IDNowProxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDNowProxy/PublicHeader.h>
+
+
